f5a2612079bed3b96148f5d69f2875a4d9b047c2
Maxwell9-9 <Chukwudumebi.Anaruagu@edu.sait.ca>
2025-12-11 16:04:25 -0700

    movie id

---
+++ app/movie/[id]/page.jsx
@@ -0,0 +1,44 @@
+"use client"
+
+import Link from "next/link";
+import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default function Movie() {
+    const params = useParams();
+    const [movie, setMovie] = useState({});
+    const [loaded, setLoaded] = useState(false);
+
+    useEffect(() => {
+
+        // get the popular movies
+        const options = {
+            method: 'GET',
+            headers: {
+                accept: 'application/json',
+                Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_ACCESS_TOKEN}`
+            }
+        };
+        fetch(`https://api.themoviedb.org/3/movie/${params.id}?language=en-US`, options)
+            .then(res => res.json())
+            .then(data => {console.log(data); setMovie(data); setLoaded(true)})
+            .catch(err => console.error(err));
+    }, []);
+
+    if (loaded) {
+        return (
+            <main>
+                <Link href={movie.homepage}><h1 className="text-4xl">{movie.title}</h1></Link>
+                
+                <h2>{movie.release_date.split("-")[0]}</h2>
+                <h2>{movie.tagline}</h2>
+                <p>{movie.overview}</p>
+            </main>
+            
+        );
+    } else {
+        return (
+            <p>loading...</p>
+        )
+    }
+}
