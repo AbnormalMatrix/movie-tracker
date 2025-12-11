7b68bf07c34117e7e532d43a2865d4c91eb6bbca
AbnormalMatrix <babin.carter@gmail.com>
2025-12-11 13:18:16 -0700

    add a page for the movies with dynamic routes

--- app/popular/page.jsx
+++ app/popular/page.jsx
@@ -1,5 +1,6 @@
 "use client"
 
+import Link from "next/link";
 import { useEffect, useState } from "react"
 
 
@@ -25,13 +26,25 @@ export default function Page() {
 
 
     return (
-        <div>
+        <div className="grid grid-cols-2 gap-4">
             {movies.map((movie) => (
-                <p key={movie.id}>
-                    {movie.title}
-                    <img src={"https://image.tmdb.org/t/p/w500/" + movie.backdrop_path} />
-                </p>
+                <Link href={`/movie/${movie.id}`} key={movie.id}>
+                    <div className="relative">
+                        <img
+                        src={`https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`}
+                        className="w-full h-auto rounded-lg"
+                        />
+
+                        {/* Title overlay */}
+                        <div className="absolute bottom-0 left-0 w-full bg-black/60 text-white p-2 rounded-b-lg">
+                        {movie.title}
+                        </div>
+                    </div>
+                </Link>
+
             ))}
         </div>
-    )
+    );
+
+    
 }
